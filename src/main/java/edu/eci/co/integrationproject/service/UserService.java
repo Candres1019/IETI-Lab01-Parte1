e77e1ba1f7f983fe650cd2f9be6966109a282488
Andres Calderon <andres.calderon@payu.com>
2021-08-19 17:59:41 -0500

    Creation of UserService interface

---
+++ src/main/java/edu/eci/co/integrationproject/service/UserService.java
@@ -0,0 +1,21 @@
+package edu.eci.co.integrationproject.service;
+
+import java.util.List;
+
+import edu.eci.co.integrationproject.data.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+
+	User create(User user);
+
+	User findById(String id);
+
+	List<User> all();
+
+	void deleteById(String id);
+
+	User update(User user, String userId);
+
+}
