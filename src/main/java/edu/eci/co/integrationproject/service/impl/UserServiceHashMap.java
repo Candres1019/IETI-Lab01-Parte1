c654eefbe221a3afac8a1985f512ed2a1e9fdc34
Andres Calderon <andres.calderon@payu.com>
2021-08-20 12:04:27 -0500

    Addition of the AtomicInteger for the assignment of the userId and change in the methods

--- src/main/java/edu/eci/co/integrationproject/service/impl/UserServiceHashMap.java
+++ src/main/java/edu/eci/co/integrationproject/service/impl/UserServiceHashMap.java
@@ -3,8 +3,10 @@ package edu.eci.co.integrationproject.service.impl;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import edu.eci.co.integrationproject.data.User;
+import edu.eci.co.integrationproject.exception.UserServiceException;
 import edu.eci.co.integrationproject.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -13,20 +15,22 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class UserServiceHashMap implements UserService {
 
+	private static final AtomicInteger userIdCounter = new AtomicInteger(1);
 	private final HashMap<String, User> userHashMap;
 
 	@Override public User create(final User user) {
 
+		user.setId(String.valueOf(userIdCounter.getAndIncrement()));
 		userHashMap.put(user.getId(), user);
 		return user;
 	}
 
-	@Override public User findById(final String id) {
+	@Override public User findById(final String id) throws UserServiceException {
 
 		if (userHashMap.containsKey(id)) {
 			return userHashMap.get(id);
 		}
-		return null;
+		throw new UserServiceException(UserServiceException.USER_NOT_FOUND);
 	}
 
 	@Override public List<User> all() {
@@ -41,9 +45,8 @@ public class UserServiceHashMap implements UserService {
 
 	@Override public User update(final User user, final String userId) {
 
-		if (userHashMap.containsKey(userId)) {
-			return userHashMap.put(userId, user);
-		}
-		return null;
+		user.setId(userId);
+		userHashMap.put(userId, user);
+		return user;
 	}
 }
